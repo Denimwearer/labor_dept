f7cb226b712debeca1e808c2a9ff6b469fe5c761
Cordero Dixon <CorderoDixon12@yahoo.com>
2023-02-06 18:08:45 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  root to: 'site#index'
   
   get '/people', to: 'people#index'
   get '/people/:id', to: 'people#show'
